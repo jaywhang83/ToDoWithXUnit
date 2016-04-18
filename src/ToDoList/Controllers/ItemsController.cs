64b4934eae76228804a64cffa2a5c0958fde89ce
Andrew Cho and Chris kuiper <cho-andrew@hotmail.com>
2016-04-18 09:16:49 -0700

    add functionality to show views of items and details of items

---
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+    }
+}
--- src/ToDoList/Startup.cs
+++ src/ToDoList/Startup.cs
@@ -6,22 +6,42 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.Entity;
+using ToDoList.Models;
 
 namespace ToDoList
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<ToDoListContext>(options =>
+                options.UseSqlServer(Configuration["Data:DefaultConnectionString"]));
         }
+
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
