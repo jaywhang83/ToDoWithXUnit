0cdc44f4e114078506232d24640968a545fd7172
Chris Kuiper <chris.kuiper@gmail.com>
2016-04-19 09:01:31 -0700

    Create Views and format them as table index

--- src/ToDoList/Models/Category.cs
+++ src/ToDoList/Models/Category.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ToDoList.Models
 {
+    [Table("Categories")]
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
     }
 }
