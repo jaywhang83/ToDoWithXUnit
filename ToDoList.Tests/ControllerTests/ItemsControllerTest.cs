2f7f2c94f2890d7bd8f3cfe98692b89003668658
Chris Kuiper and Jay Whang <chris.kuiper@gmail.com and jaywhang83@gmail.com>
2016-05-02 10:49:36 -0700

    Add mock database to tests

--- ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -1,31 +1,70 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNet.Mvc;
-using Microsoft.Data.Entity;
+﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
 using ToDoList.Controllers;
 using ToDoList.Models;
 using Xunit;
-using System;
+using System.Linq;
+using Moq;
 
 namespace ToDoList.Tests
 {
     public class ItemsControllerTest
     {
+        Mock<IItemRepository> mock = new Mock<IItemRepository>();
+
+        private void DbSetup()
+        {
+            mock.Setup(m => m.Items).Returns(new Item[]
+            {
+                new Item {ItemId = 1, Description = "Wash the dog" },
+                new Item {ItemId = 2, Description = "Do the dishes" },
+                new Item {ItemId = 3, Description = "Sweep the floor" }
+            }.AsQueryable());
+        }
 
         [Fact]
-        public void Post_MethodAddsItem_Test()
+        public void Mock_GetViewResultIndex_Test() //Confirms route returns view
         {
             //Arrange
-            ItemsController controller = new ItemsController();
-            Item testItem = new Item();
-            testItem.Description = "test item";
-            testItem.CategoryId = 1;
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
 
-            // Act
-            controller.Create(testItem);
-            ViewResult indexView = new ItemsController().Index() as ViewResult;
-            var collection = indexView.ViewData.Model as IEnumerable<Item>;
+            //Act
+            var result = controller.Index();
 
             //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Mock_IndexListOfItems_Test() //Confirms model as list of items
+        {
+            // Arrange
+            DbSetup();
+            ViewResult indexView = new ItemsController(mock.Object).Index() as ViewResult;
+
+            // Act
+            var result = indexView.ViewData.Model;
+
+            // Assert
+            Assert.IsType<List<Item>>(result);
+        }
+
+        [Fact]
+        public void Mock_ConfirmEntry_Test() //Confirms presence of known entry
+        {
+            // Arrange
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
+            Item testItem = new Item();
+            testItem.Description = "Wash the dog";
+            testItem.ItemId = 1;
+
+            // Act
+            ViewResult indexView = controller.Index() as ViewResult;
+            var collection = indexView.ViewData.Model as IEnumerable<Item>;
+
+            // Assert
             Assert.Contains<Item>(testItem, collection);
         }
     }
