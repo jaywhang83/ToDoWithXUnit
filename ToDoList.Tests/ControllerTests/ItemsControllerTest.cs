5751a8770168f926b77fb4ed7a2287757eb6595b
Chris Kuiper <chris.kuiper@gmail.com>
2016-05-02 08:47:29 -0700

    add tests for model and controllers

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using Xunit;
+
+namespace ToDoList.Tests
+{
+    public class ItemsControllerTest
+    {
+        [Fact]
+        public void Get_ViewRestult_Index_Test()
+        {
+            //Arrange
+            ViewResult indexView = new ItemsController().Index() as ViewResult;
+
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsType<List<Item>>(result); 
+        }
+    }
+}
